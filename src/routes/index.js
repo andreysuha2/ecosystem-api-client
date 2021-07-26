358775790b183fc5962f3b336c8978eb4d00274e
Andrey Sukhenko <andreysuha2@gmail.com>
2021-07-26 17:37:56 +0300

    update path

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ import Http from '@http/api/auth';
 
 export default (app) => {
     app.get('/test', (req, res) => {
-        res.json({ "url": process.env.API_URL })
+        const http = new Http(req, res);
+        http.check()
+            .then(({ data }) => res.json({ data, http }))
+            .catch(({ data }) => res.json(data));
     });
 }
